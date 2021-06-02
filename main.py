5a0ab3e9ec957ec1b1df8638f2ccc8b37e2746c5
maxmiles <maxmiles@uw.edu>
2021-06-02 15:52:00 -0700

    testing discord connection

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    
+    print(f'{client.user} has connected to Discord!')
+    
 
 @client.event
 async def on_message(message):
@@ -15,4 +18,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv('ODQ5NTU5MjMwNDE5MzA0NDY5.YLc7sA.TmPEeMf6MgsiCFzbRDDmm6kqnho'))
