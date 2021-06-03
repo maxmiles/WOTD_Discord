380de5bb1ff54ae47e7061b27cabcce8d1e59001
maxmiles <maxmiles@uw.edu>
2021-06-03 00:26:06 -0700

    began on_typing event listener

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ client = discord.Client()
 # Alert when ready 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
     print(f'{client.user} has connected to Discord!')
     
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
+# Default hello message response
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if message.content.startswith('!word'):
-        await message.channel.send('!word')
+            await message.channel.send('!word')
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')  
+
+# # If Kevin is typing
+# @client.event
+# async def on_typing(channel, user):
+#     if user != client.user:
+#         await channel.send('stfu')
+
 
 TOKEN = config("TOKEN")
 client.run(TOKEN)
